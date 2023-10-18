0ca058aa0811b3f2a85abe25fc793309ba26c918
Smek-one <bertrandbovet@gmail.com>
2023-10-18 06:56:29 +0200

    changing like unlike buttons style

--- components/LikeUnlike.tsx
+++ components/LikeUnlike.tsx
@@ -1,15 +1,25 @@
 "use client"
-import { Provider, LikeButton, ClapButton } from "@lyket/react";
+import { Provider, LikeButton, UpdownButton } from "@lyket/react";
 
 export default function LikeUnkije(){
     return(
-        <Provider apiKey="pt_8826ea21fd44bf6839973ac7fb59ee">
+        <Provider apiKey="pt_8826ea21fd44bf6839973ac7fb59ee"
+        theme={{
+            colors: {
+              icon: "#116A7B",
+              text: "#116A7B",
+              primary: "#CDC2AE",
+              secondary: "CDC2AE",
+              highlight: "#116A7B"
+            }
+          }}
+        >
             <div className="text-center mx-auto likeButton">
               <h3 className="text-center">Si tu aimes ce post, n'hésite pas à liker :</h3>
-          <LikeButton 
+          <UpdownButton 
             namespace="testing-react"
             id="everybody-like-now"
-            component={LikeButton.templates.Twitter}
+            component={UpdownButton.templates.Simple}
           />
           </div>
         </Provider>
